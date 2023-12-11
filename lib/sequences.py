83036afaa140b469f25c5bc9ae32abc94101c7c2
Njugunasam <sammynjuguna5826@gmail.com>
2023-12-11 16:56:30 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = [0, 1]
+
+    for _ in range(2, length):
+        other_number = sequence[-1] + sequence[-2]
+        sequence.append(other_number)
+
+    print(sequence[:length])
